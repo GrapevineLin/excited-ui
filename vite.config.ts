7b92afdb23f4f804bcbab9841cbad10142ca4dcd
GrapevineLin <546415457@qq.com>
2022-08-25 23:17:06 +0800

    fix: vite添加base配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig({
+    base: '/excited-ui/',
     resolve: {
         alias: {
             'vue': 'vue/dist/vue.esm-bundler.js' // 定义vue的别名，如果使用其他的插件，可能会用到别名
