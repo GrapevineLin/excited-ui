3569551d4f7df70218e98ffd51d0ff4427ce53d7
GrapevineLin <546415457@qq.com>
2022-09-10 20:59:01 +0800

    chore: change package.json info

--- scripts/ci-upload.js
+++ scripts/ci-upload.js
@@ -1,40 +1,45 @@
-const axios = require('axios')
-const dayjs = require('dayjs')
-const config = require('../package.json')
+const axios = require("axios");
+const dayjs = require("dayjs");
+const config = require("../packages/core/package.json");
 
-    ; (async () => {
-        try {
-            const { version } = config
-            const buildTime = dayjs().format('YYYY-MM-DD HH:mm:ss')
-            const desc = `构建时间-${buildTime}`
+(async () => {
+  try {
+    const { version } = config;
+    const buildTime = dayjs().format("YYYY-MM-DD HH:mm:ss");
+    const desc = `构建时间-${buildTime}`;
 
-            await sendMsgByWebhook({ version, desc })
-        } catch (e) {
-            console.log(`===================脚本报错↓===================`)
-            console.log(e)
-            console.log(`===================脚本报错↑===================`)
-            process.exit(-1)
-        }
-    })();
+    await sendMsgByWebhook({ version, desc });
+  } catch (e) {
+    console.log(`===================脚本报错↓===================`);
+    console.log(e);
+    console.log(`===================脚本报错↑===================`);
+    process.exit(-1);
+  }
+})();
 
-async function sendMsgByWebhook (msg) {
-    const { version, desc } = msg    
-    const WEBHOOK_KEY = process.env.WEBHOOK_KEY
-    const COMMIT_MESSAGE = process.env.COMMIT_MESSAGE
-    const COMMITTER = process.env.COMMITTER
-    const { data: { errcode, errmsg } } = await axios.post(`https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${WEBHOOK_KEY}`, {
-        msgtype: "markdown",
-        markdown: {
-            content: `构建通知：<font color=\"warning\">[ExcitedUI](https://grapevinelin.github.io/excited-ui/)</font>-${version}\n
-             >描述：<font color=\"info\">${desc}</font>
-             >message：<font color=\"info\">${COMMIT_MESSAGE}</font>
-             >committer：<font color=\"info\">${COMMITTER}</font>`
-        }
-    })
-    if (errcode === 0) {
-        console.log('推送成功')
-    } else {
-        const errMsg = `推送失败：\ncode:${errcode}\nmsg:${errmsg}`
-        throw errMsg
+async function sendMsgByWebhook(msg) {
+  const { version, desc } = msg;
+  const WEBHOOK_KEY = process.env.WEBHOOK_KEY;
+  const COMMIT_MESSAGE = process.env.COMMIT_MESSAGE;
+  const COMMITTER = process.env.COMMITTER;
+  const {
+    data: { errcode, errmsg },
+  } = await axios.post(
+    `https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${WEBHOOK_KEY}`,
+    {
+      msgtype: "markdown",
+      markdown: {
+        content: `构建通知：<font color="warning">[ExcitedUI](https://grapevinelin.github.io/excited-ui/)</font>-${version}\n
+             >描述：<font color="info">${desc}</font>
+             >message：<font color="info">${COMMIT_MESSAGE}</font>
+             >committer：<font color="info">${COMMITTER}</font>`,
+      },
     }
+  );
+  if (errcode === 0) {
+    console.log("推送成功");
+  } else {
+    const errMsg = `推送失败：\ncode:${errcode}\nmsg:${errmsg}`;
+    throw errMsg;
+  }
 }
